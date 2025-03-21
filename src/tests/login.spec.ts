9c6517d20c2861f10c19ce920d0cb8fc7cfdddb8
087065078085083 <wanus.kurirung@gmail.com>
2025-03-21 11:32:12 +0700

    Add test file login.spec.ts

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -1,9 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('[data-test="title"]')).toBeVisible();
+  await page.goto('http://localhost:3000/products');
+  await page.locator('.w-full').first().click();
+  await page.getByRole('link', { name: 'Cart' }).click();
+  await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
+  await page.locator('input[name="customerName"]').click();
+  await page.locator('input[name="customerName"]').fill('Example');
+  await page.locator('input[name="customerTel"]').click();
+  await page.locator('input[name="customerTel"]').fill('0812345678');
+  await page.locator('input[name="customerEmail"]').click();
+  await page.locator('input[name="customerEmail"]').fill('user@example.com');
+  await page.getByRole('button', { name: 'Complete Order' }).click();
+  // await page.getByText('Select Image').click();
+  // await page.getByText('ODTS RETAILCartPaymentOrder').setInputFiles('./test_src/u-i-i-ai-o-uu-ii-a-i.jpeg');
+  const fileInput = await page.locator('input[type="file"]');
+  await fileInput.setInputFiles('src/tests/test_src/u-i-i-ai-o-uu-ii-a-i.jpeg');
+
+  await page.getByRole('button', { name: 'Complete Payment' }).click();
+  await page.getByRole('link', { name: 'กลับสู่หน้าหลัก' }).click();
 });
