63265e8fd52140a05e8bb5ea01c0af46445c9e85
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-03-21 15:53:47 +0700

    Add confirm order input is success

--- src/page-object/confirm-order-page.ts
+++ src/page-object/confirm-order-page.ts
@@ -1,26 +1,32 @@
-import { Page, Locator } from "playwright";
+import { Page, Locator } from "playwright"
+import { expect } from "@playwright/test"
 export class ConfirmOrderPage {
-    readonly page: Page
-    readonly customerName: Locator
-    readonly customerTel: Locator
-    readonly customerEmail: Locator
-    readonly submitButton: Locator
+  readonly page: Page
+  readonly customerName: Locator
+  readonly customerTel: Locator
+  readonly customerEmail: Locator
+  readonly submitButton: Locator
 
-    constructor(page: Page) {
-        this.page = page
-        this.customerName = this.page.locator('input[name="customerName"]')
-        this.customerTel = this.page.locator('input[name="customerTel"]')
-        this.customerEmail = this.page.locator('input[name="customerEmail"]')
-        this.submitButton = this.page.locator('input[type="submit"]')
-    }
+  constructor(page: Page) {
+    this.page = page
+    this.customerName = this.page.locator('input[name="customerName"]')
+    this.customerTel = this.page.locator('input[name="customerTel"]')
+    this.customerEmail = this.page.locator('input[name="customerEmail"]')
+    this.submitButton = this.page.getByText('Complete Order')
+  }
 
-    async fillInput(){
-        await this.customerName.fill("Bunyakorn")
-        await this.customerTel.fill("0800680597")
-        await this.customerEmail.fill("bunyakorn")
-    }
+  async showOrderConfirmPage() {
+    await expect(this.page.getByText("Confirm Your Order")).toBeVisible()
+  }
 
-    async confirmOrder() {
-        await this.submitButton.click()
-    }
+  async fillInput() {
+    await this.customerName.fill("Bunyakorn")
+    await this.customerTel.fill("0800680597")
+    await this.customerEmail.fill("bunyakorn")
+  }
+
+  async confirmOrder() {
+    await this.submitButton.scrollIntoViewIfNeeded();
+    await this.submitButton.click()
+  }
 }
